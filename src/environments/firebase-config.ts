8521f24a751ca25de30d62006b3f68c493d3e928
Matheus OS <matheus.sdoliveira@gmail.com>
2025-06-01 20:21:45 -0300

    firebase config

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCovLSskxY-vIlHh8ZxJmsPkoThNRGSSjQ",
+  authDomain: "ionic-news-1e292.firebaseapp.com",
+  projectId: "ionic-news-1e292",
+  storageBucket: "ionic-news-1e292.firebasestorage.app",
+  messagingSenderId: "941934738848",
+  appId: "1:941934738848:web:c61e0b6ac9b1d24018f8c5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
